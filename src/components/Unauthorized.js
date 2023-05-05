c5d7060c13e452e175db7877a509dad8c07fd25c
beelzekamibub <chait8126@gmail.com>
2023-05-05 15:43:06 +0530

    role based authorization not jwt token

--- src/components/Unauthorized.js
+++ src/components/Unauthorized.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Unauthorized = () => {
+    const navigate = useNavigate();
+    const goback = () => navigate(-1);
     return (
-        <div>Unauthorized</div>
+        <section>
+            <h1>Unauthorized</h1>
+            <br />
+            <p>You dont have acces to go to this page.</p>
+            <div className="GoBackFlexGrow">
+                <button onClick={goback}> Go Back </button>
+            </div>
+
+        </section>
     )
 }
 
